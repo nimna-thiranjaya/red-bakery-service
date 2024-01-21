9a5bd41cb0b8d6e44a69713999a3745ac67f727c
nimna-thiranjaya <nimnathiranjaya523@gmail.com>
2024-01-21 19:55:58 +0530

    initial configurations

---
+++ src/main/java/com/redbakery/redbakeryservice/RedBakeryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.redbakery.redbakeryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedBakeryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedBakeryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/redbakery/redbakeryservice/RedBakeryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redbakery.redbakeryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedBakeryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
